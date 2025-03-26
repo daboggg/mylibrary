642f4bdb50bb719e5841b37e9f51c41ebb795460
vzini <vzinin@list.ru>
2025-03-26 14:43:34 +0300

    update FusionBrainAPI

--- library/kandinsky.py
+++ library/kandinsky.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("KANDINSKY_SECRET_KEY")
 log = logging.getLogger(__name__)
 
 
-class Text2ImageAPI:
+class FusionBrainAPI:
 
     def __init__(self, url, api_key, secret_key):
         self.URL = url
@@ -23,49 +23,47 @@ class Text2ImageAPI:
             'X-Secret': f'Secret {secret_key}',
         }
 
-    def get_model(self):
-        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+    def get_pipeline(self):
+        response = requests.get(self.URL + 'key/api/v1/pipelines', headers=self.AUTH_HEADERS)
         data = response.json()
         return data[0]['id']
 
-    def generate(self, prompt, model, images=1, width=768, height=1024):
+    def generate(self, prompt, pipeline, images=1, width=768, height=1024):
         params = {
             "type": "GENERATE",
             "numImages": images,
             "width": width,
             "height": height,
             "generateParams": {
-                "query": f"{prompt}"
-            },
-            "images": ["string"]
+                "query": prompt
+            }
         }
 
         data = {
-            'model_id': (None, model),
+            'pipeline_id': (None, pipeline),
             'params': (None, json.dumps(params), 'application/json')
         }
-        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        response = requests.post(self.URL + 'key/api/v1/pipeline/run', headers=self.AUTH_HEADERS, files=data)
         data = response.json()
+
         return data['uuid']
 
     def check_generation(self, request_id, attempts=10, delay=10):
         while attempts > 0:
-            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            response = requests.get(self.URL + 'key/api/v1/pipeline/status/' + request_id, headers=self.AUTH_HEADERS)
             data = response.json()
             if data['status'] == 'DONE':
-                return data['images']
+                return data['result']['files']
 
             attempts -= 1
             time.sleep(delay)
 
-
 def get_image_from_kandinsky(prompt:str):
-
-    api = Text2ImageAPI('https://api-key.fusionbrain.ai/', API_KEY, SECRET_KEY)
-    model_id = api.get_model()
+    api = FusionBrainAPI('https://api-key.fusionbrain.ai/', API_KEY, SECRET_KEY)
+    pipeline_id = api.get_pipeline()
     log.info(time.ctime())
-    uuid = api.generate(prompt, model_id)
-    images = api.check_generation(uuid)
+    uuid = api.generate(prompt, pipeline_id)
+    files = api.check_generation(uuid)
 
     log.info(time.ctime())
-    return images[0]
+    return files[0]
